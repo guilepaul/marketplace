2062110827cfe6c84a5600552c271b0d93c8d892
Guilherme Di Paula <guilepaul@gmail.com>
2020-11-23 00:04:43 -0300

    decrement asyncstorage

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -75,16 +75,30 @@ const CartProvider: React.FC = ({ children }) => {
       setProducts(newProducts);
 
       await AsyncStorage.setItem(
-        'GoMarketplace:products',
+        '@GoMarketplace:products',
         JSON.stringify(newProducts),
       );
     },
     [products],
   );
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const decrement = useCallback(
+    async id => {
+      const newProducts = products.map(product =>
+        product.id === id
+          ? { ...product, quantity: product.quantity - 1 }
+          : product,
+      );
+
+      setProducts(newProducts);
+
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
